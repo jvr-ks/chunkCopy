652d001e7e93dc3b83d9be2a73a8396cda847bd3
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2024-06-11 11:57:58 +0200

    0.022

--- updater_default_files$_$_$64.bat
+++
@@ -1,8 +0,0 @@
-@rem updater_default_files$_$_$64.bat
-
-xcopy /Y /I "chunkCopy.exe" "chunkCopy.exe.default"
-
-xcopy /Y /I "chunkCopy.ini" "chunkCopy.ini.default"
-
-xcopy /Y /I "license.txt" "license.txt.default"
-
